57cff0199b26133fc7253c59d052296233b1f22e
yasmeen <yasmynjad53@gmail.com>
2023-11-20 18:26:24 +0200

    add rich text in sura details screen

--- lib/controller/locale/change_lang.dart
+++ lib/controller/locale/change_lang.dart
@@ -5,13 +5,13 @@ import 'package:get/get.dart';
 import '../../main.dart';
 
 class ChangeLang extends GetxController {
-  Locale initial = prefs!.getString("lang") == null
+  Locale initial = prefs.getString("lang") == null
       ? Get.deviceLocale!
       : Locale(prefs.getString("lang")!);
 
   void changeLang(String lang) {
     Locale locale = Locale(lang);
-    prefs!.setString("lang", locale.toString());
+    prefs.setString("lang", locale.toString());
     Get.updateLocale(locale);
   }
 }
--- lib/screens/sura_details.dart
+++ lib/screens/sura_details.dart
@@ -1,3 +1,138 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:get/get.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:islamy/controller/theming.dart';
+
+// import 'package:islamy/models/sura_model.dart';
+
+// class SuraDetails extends StatefulWidget {
+//   SuraDetails({super.key});
+
+//   static const String routeName = "suraDetails";
+
+//   @override
+//   State<SuraDetails> createState() => _SuraDetailsState();
+// }
+
+// class _SuraDetailsState extends State<SuraDetails> {
+//   List<String> verses = [];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var args = ModalRoute.of(context)?.settings.arguments as SuraModel;
+//     if (verses.isEmpty) {
+//       loadFile(args.index);
+//     }
+//     return Stack(
+//       children: [
+//         Get.isDarkMode
+//             ? Image.asset(
+//                 "assets/images/dark_bg.png",
+//                 width: double.infinity,
+//                 fit: BoxFit.cover,
+//               )
+//             : Image.asset(
+//                 "assets/images/default_bg.png",
+//                 width: double.infinity,
+//                 fit: BoxFit.cover,
+//               ),
+//         Scaffold(
+//             appBar: AppBar(
+//                 title:
+//                     Text("1".tr, style: Theme.of(context).textTheme.bodyLarge)),
+//             body: Padding(
+//               padding: const EdgeInsets.all(12),
+//               child: Card(
+//                 color: Get.isDarkMode
+//                     ? MyThemeData.primaryColordark
+//                     : MyThemeData.whiteColor,
+//                 elevation: 8,
+//                 shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(25),
+//                     side: BorderSide(
+//                         color: Get.isDarkMode
+//                             ? MyThemeData.secondColor
+//                             : MyThemeData.primaryColor)),
+//                 child: Column(
+//                   children: [
+//                     SizedBox(
+//                       height: 12,
+//                     ),
+//                     Row(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: [
+//                         Text(
+//                           "${args.name}",
+//                           style: Theme.of(context)
+//                               .textTheme
+//                               .bodyLarge!
+//                               .copyWith(
+//                                   color: Get.isDarkMode
+//                                       ? MyThemeData.secondColor
+//                                       : MyThemeData.blackColor),
+//                         ),
+//                         SizedBox(
+//                           width: 24,
+//                         ),
+//                         ImageIcon(
+//                             AssetImage(
+//                               "assets/images/play-circle.png",
+//                             ),
+//                             color: Get.isDarkMode
+//                                 ? MyThemeData.secondColor
+//                                 : MyThemeData.primaryColor,
+//                             size: 27)
+//                       ],
+//                     ),
+//                     Divider(
+//                       thickness: 2,
+//                       color: Get.isDarkMode
+//                           ? MyThemeData.secondColor
+//                           : MyThemeData.primaryColor,
+//                       indent: 40,
+//                       endIndent: 40,
+//                     ),
+//                     Expanded(
+//                       child: Padding(
+//                         padding: const EdgeInsets.all(8.0),
+//                         child: ListView.builder(
+//                           itemBuilder: (context, index) {
+//                             return Directionality(
+//                               textDirection: TextDirection.rtl,
+//                               child: RichText(
+//                                 textAlign: TextAlign.center,
+//                                 text: TextSpan(
+//                                   text: '${verses[index]}{${index + 1}}',
+//                                   style: GoogleFonts.amiriQuran(
+//                                     color: Get.isDarkMode
+//                                         ? MyThemeData.secondColor
+//                                         : MyThemeData.blackColor,
+//                                     fontSize: 20,
+//                                   ),
+//                                 ),
+//                               ),
+//                             );
+//                           },
+//                           itemCount: verses.length,
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             )),
+//       ],
+//     );
+//   }
+
+//   void loadFile(int index) async {
+//     String file = await rootBundle.loadString("assets/files/${index + 1}.txt");
+//     List<String> lines = file.split("\n");
+//     verses = lines;
+//     setState(() {});
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -17,6 +152,7 @@ class SuraDetails extends StatefulWidget {
 
 class _SuraDetailsState extends State<SuraDetails> {
   List<String> verses = [];
+  bool isLoading = true; // تعيين القيمة الافتراضية للتحميل
 
   @override
   Widget build(BuildContext context) {
@@ -39,8 +175,8 @@ class _SuraDetailsState extends State<SuraDetails> {
               ),
         Scaffold(
             appBar: AppBar(
-                title: Text("إسلامي",
-                    style: Theme.of(context).textTheme.bodyLarge)),
+                title:
+                    Text("1".tr, style: Theme.of(context).textTheme.bodyLarge)),
             body: Padding(
               padding: const EdgeInsets.all(12),
               child: Card(
@@ -94,26 +230,37 @@ class _SuraDetailsState extends State<SuraDetails> {
                       endIndent: 40,
                     ),
                     Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListView.builder(
-                          itemBuilder: (context, index) {
-                            return Directionality(
-                              textDirection: TextDirection.rtl,
-                              child: Text(
-                                "${verses[index]}",
-                                textAlign: TextAlign.center,
-                                style: GoogleFonts.amiriQuran(
-                                    color: Get.isDarkMode
-                                        ? MyThemeData.secondColor
-                                        : MyThemeData.blackColor,
-                                    fontSize: 20),
+                      child: isLoading // إذا كان التحميل قيد التنفيذ
+                          ? Center(
+                              child: CircularProgressIndicator(
+                                  color: Get.isDarkMode
+                                      ? MyThemeData.secondColor
+                                      : MyThemeData
+                                          .primaryColor), // عرض مؤشر التقدم الدائري
+                            )
+                          : Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ListView.builder(
+                                itemBuilder: (context, index) {
+                                  return Directionality(
+                                    textDirection: TextDirection.rtl,
+                                    child: RichText(
+                                      textAlign: TextAlign.center,
+                                      text: TextSpan(
+                                        text: '${verses[index]}(${index + 1})',
+                                        style: GoogleFonts.amiriQuran(
+                                          color: Get.isDarkMode
+                                              ? MyThemeData.secondColor
+                                              : MyThemeData.blackColor,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                                itemCount: verses.length,
                               ),
-                            );
-                          },
-                          itemCount: verses.length,
-                        ),
-                      ),
+                            ),
                     ),
                   ],
                 ),
@@ -125,8 +272,10 @@ class _SuraDetailsState extends State<SuraDetails> {
 
   void loadFile(int index) async {
     String file = await rootBundle.loadString("assets/files/${index + 1}.txt");
-    List<String> lines = file.trim().split("\n");
+    List<String> lines = file.split("\n");
     verses = lines;
-    setState(() {});
+    setState(() {
+      isLoading = false; // انتهاء التحميل
+    });
   }
 }
